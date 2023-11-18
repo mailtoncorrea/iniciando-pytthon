485c8a3d854bc9c56d9e13a8ce755be2be59c7d9
devMailton <benicorrea2@gmail.com>
2023-11-18 20:43:37 -0300

    iniciando no python

---
+++ aula.py
@@ -0,0 +1,5 @@
+peso = eval(input('entre com seu peso: '))
+altura = eval(input("entre com a sua altura: "))
+imc = peso/(altura**2)
+print('imc = ', imc)
+#como solicitar ao usuario para fonecer um programa de massa corporea
---
+++ aulahoras.py
@@ -0,0 +1,10 @@
+""""hora = 10
+minutos = 20
+segundos = 22
+print(str(hora) + ' : ' + str(minutos) + ' : ' + str(segundos))"""
+#exemplo de data e hora
+
+hora = 10
+minutos = 30
+segundos = 50
+print("{} : {} : {}". format(hora, minutos, segundos))
